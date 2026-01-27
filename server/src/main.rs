0157982883e940523b52aba6b53c5695b245faae
Jordan Wright <jaw566@nau.edu>
2026-01-26 22:18:08 -0500

    added CSS style sheet and three side by side boxes hold body content; writes the correct number words known even when word provided is not in language

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,7 @@
-use std::{
-    fs,
-    io::{BufReader, prelude::*},
-    net::{TcpListener, TcpStream},
-    collections::HashMap,
-};
-use rusqlite::{
-    Connection,
-    params,
-};
-use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
-use lingua::Language::{English, Spanish};
+use std::{fs,io::{BufReader,prelude::*},net::{TcpListener,TcpStream},collections::HashMap,};
+use rusqlite::{Connection,params,};
+use lingua::{Language, LanguageDetector, LanguageDetectorBuilder,};
+use lingua::Language::{Spanish, English,};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -40,27 +32,26 @@ fn handle_connection(mut stream: TcpStream, conn: &mut Connection) {
     println!("line: {request_line:#?}");
 
     let (status_line, filename) = if request_line.contains("GET / HTTP/1.1") {
-        ("HTTP/1.1 200 OK", "words_page.html")
+        ("HTTP/1.1 200 OK", "welcome.html")
     } else if request_line.contains("POST /save HTTP/1.1") {
         ("HTTP/1.1 200 OK", "saved_page.html")
+    } else if request_line.contains("GET /style.css HTTP/1.1") {
+        ("HTTP/1.1 200 OK", "style.css")
     } else {
         ("HTTP/1.1 400 NOT FOUND", "404.html")
     };
 
-    // User is requesting to save word
+    // User is requesting to save word or clear the database 
     if filename == "saved_page.html" {
-        // Read headers
         let mut content_length = 0usize;
 
         // Parse content length
         loop {
             let mut header = String::new();
             reader.read_line(&mut header).unwrap();
-
             if header == "\r\n" {
                 break;
             }
-
             if let Some(len) = header.strip_prefix("Content-Length: ") {
                 content_length = len.trim().parse().unwrap();
             }
@@ -69,52 +60,70 @@ fn handle_connection(mut stream: TcpStream, conn: &mut Connection) {
         // Read body
         let mut body = vec![0; content_length];
         reader.read_exact(&mut body).unwrap();
-
         let body = String::from_utf8_lossy(&body);
-
         println!("Form body: {}", body);
 
         // Parse form fields
         let params = parse_form(&body);
-        let newword = params.get("newword").unwrap();
-        println!("Word: {newword}");
-
-        let languages = vec![English, Spanish];
-        let detector: LanguageDetector = LanguageDetectorBuilder::from_languages(&languages).build();
-        let detected_language: Option<Language> = detector.detect_language_of(newword);
-        let mut save_newword: bool = false;
-        match detected_language {
-            Some(Spanish) => println!("Success: '{newword}' is Spanish!"),
-            Some(English) => println!("Warning: '{newword}' is English!"),
-            None => println!("Warning: '{newword}' is not English or Spanish!"),
-        }
-        if detected_language == Some(Spanish) {
-            save_newword = true;
-        }
-        if save_newword {
-            let _conn_res = conn.execute(
-                "INSERT INTO words (word) VALUES (?1)",
-                params![newword],
-            );
-
-            match _conn_res {
-                Ok(_conn_res) => println!("Success"),
-                Err(error) => println!("Warning: Unique words only {:?}", error),
-            }
-
-            let count = count_entries(&conn);
-            println!("Number of words known: {count}");
-            write_word_count_html("saved_page.html", count);
+        let newword = params.get("newword");
+        if newword == None {
+            conn.execute("DROP TABLE IF EXISTS words", ()).unwrap();
+            write_word_count_html("saved_page.html", 0);
+        } else {
+            detect_and_save(newword.unwrap(), conn);
+            let w = newword.unwrap();
+            println!("Word: {w}");
         }
     }
 
     // Respond
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-    let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn detect_and_save(newword: &String, conn: &mut Connection) {
+    let languages = vec![Spanish, English];
+    let detector: LanguageDetector = 
+        LanguageDetectorBuilder::from_languages(&languages).build();
+    let detected_language: Option<Language> = detector.detect_language_of(newword);
+    let mut save_newword: bool = false;
+
+    match detected_language {
+        Some(Spanish) => {
+            let confidence = detector.compute_language_confidence(newword, Spanish);
+            let rounded_confidence = (confidence * 100.0).round() / 100.0;
+            println!("Success: '{newword}' is Spanish!");
+            println!("Confidence value = {rounded_confidence}");
+            if rounded_confidence > 0.5 {
+                save_newword = true;
+            }
+        }
+        Some(English) => {
+            println!("Warning: '{newword}' is English!");
+            let confidence = detector.compute_language_confidence(newword, Spanish);
+            let rounded_confidence = (confidence * 100.0).round() / 100.0;
+            println!("Confidence value = {rounded_confidence}");
+        }
+        None => println!("Warning: '{newword}' is not English or Spanish!"),
+    }
+
+    if save_newword {
+        let _conn_res = conn.execute(
+            "INSERT INTO words (word) VALUES (?1)",
+            params![newword.to_lowercase()],
+        );
+        match _conn_res {
+            Ok(_conn_res) => println!("Success"),
+            Err(error) => println!("Warning: Unique words only {:?}", error),
+        }
+    }
+
+    let count = count_entries(&conn);
+    println!("Number of words known: {count}");
+    write_word_count_html("saved_page.html", count);
 }
 
 fn write_word_count_html(path: &str, count: i64) {
@@ -132,17 +141,31 @@ fn write_word_count_html(path: &str, count: i64) {
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Hello!</title>
+    <title>Word Count</title>
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <h1>Saved!</h1>
-    <h2>"~2000 words + rules = fluency"</h2>
+    <h1 style="font-weight:bold;text-align:center;background-color:powderblue;border:3px solid black;">Word Count!
+    <p>Track how many words you know in a foreign language</p>
+    </h1>
+    <h2>Motivation: "~2000 words + rules = fluency"</h2>
       <form action="/save" method="post">
-        <label>Enter word:</label><br>
-        <input type="text" name="newword">
-        <button type="submit">Save</button>
+        <label style="font-size:30px;">Enter word:</label><br>
+        <input style="font-size:30px;" type="text" name="newword">
+        <button style="font-size:30px;" type="submit">Save</button>
       </form>
-    <h2>Number of words banked: {}</h2>
+    <body>
+    <div class="container">
+        <div class="box">Word count: <br><br>{}</div>
+        <div class="box">Word of the day:</div>
+        <div class="box">% nouns: <br>% adj: <br>%verbs:</div>
+    </div>
+    </body>
+    <h3>
+      <form action="/save" method="post">
+        <button style="font-size:30px;" name="clear" type="submit">Clear Word Count</button>
+      </form>
+    </h3>
   </body>
 </html>"#,
         count
@@ -150,7 +173,6 @@ fn write_word_count_html(path: &str, count: i64) {
         eprintln!("Failed to write HTML file '{}': {}", path, e);
     }
 }
-
 
 fn count_entries(conn: &Connection) -> i64 {
     conn.query_row(
